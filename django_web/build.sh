e221e613b8b7f56ed07465ef985c7f6af4d18776
jimin-vision <vision202518015@gmail.com>
2026-01-20 21:20:46 +0900

    추가: 어드민 계정 추가 조치
    배포 웹에서 어드민 계정 추가

--- django_web/build.sh
+++ django_web/build.sh
@@ -4,3 +4,7 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+if [ "${RUN_SUPERUSER_SETUP}" = "1" ]; then
+  python manage.py ensure_superuser
+fi
